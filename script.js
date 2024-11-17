f5c7aaf18b1d6a0ed772fc2eb92368b6f9de1d34
te5s3rakt <117922694+te5s3rakt@users.noreply.github.com>
2024-11-17 14:39:11 +0800

    Add basic math logic

--- script.js
+++ script.js
@@ -21,95 +21,212 @@ flipButton.addEventListener('click', () => {
         flipCalculator()
     });
 
-const buttons = document.querySelectorAll('button');
+// const screen = document.querySelector('.screen');
 
-buttons.forEach((button) => {
+// function printNumber(button, wrap) {
+//     if (wrap || screen.lastChild == undefined) screen.appendChild(document.createElement('span'));
+    
+//     screen.lastChild.textContent += button.textContent;
+// };
+
+// const numberButtons = document.querySelectorAll('button.number');
+
+// numberButtons.forEach((button) => {
+//         button.addEventListener('click', () => {
+//             operand += num[button.id];
+//             printScreen(button);
+//         });
+//     });
+
+// const operatorButtons = document.querySelectorAll('button.operator')
+
+// operatorButtons.forEach((button) => {
+//         button.addEventListener('click', () => {
+//             operator = button.id
+//             printScreen(button, true);
+//             evaluate();
+//         });
+//     });
+
+// const evaluateButton = document.querySelector('button.evaluate')
+
+// evaluateButton.addEventListener('click', () => {
+//     evaluate();
+// });
+
+// function evaluate() {
+//         let a = Number(result);
+//         let b = Number(operand);
+    
+//         if (operator == 'plus') a += b;
+//         if (operator == 'minus') a -= b;
+//         if (operator == 'times') a *= b;
+//         if (operator == 'divide') a /= b;
+        
+//         operator = null;
+//         operand = ''
+//         printScreen(result, true);
+//     };
+
+const numberButtons = document.querySelectorAll('button.number');
+
+numberButtons.forEach((button) => {
         button.addEventListener('click', () => {
-            const isNumber = button.classList.contains('number');
-            const isOperator = button.classList.contains('operator');
-            const isModifier = button.classList.contains('modifier');
-            const isAction = button.classList.contains('action');
-            const isEvaluate = button.classList.contains('evaluate');
-            
-            if (isNumber) collectNumbers(button.id);
-            if (isOperator) setOperator(button.id);
-            if (isModifier) applyModifier(button.id);
-            if (isAction) action(button.id);
-            if (isEvaluate) evaluate();
+            resetScreen(true);
+            collectNumber(button);
+        })
+    });
+
+const screen = document.querySelector('.screen');
+
+function collectNumber(button) {
+    const value = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9, period: '.' };
+
+    let newChar = value[button.id];
+
+    if (
+        screen.childElementCount == 0 ||
+        screen.childElementCount == 2
+    ) screen.appendChild(document.createElement('span'));
+
+    let currentValue = screen.lastChild.textContent
+
+    if (
+        currentValue.includes('.') &&
+        newChar == '.'
+    ) newChar = '';
+
+    if (
+        currentValue.length == 0 &&
+        newChar == '.'
+    ) currentValue = 0;
+
+    screen.lastChild.textContent = currentValue + newChar
+};
+
+const operatorButtons = document.querySelectorAll('button.operator')
+
+operatorButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            resetScreen();
+            collectOperand(button);
         });
     });
 
-const num = {
-    zero: 0,
-    one: 1,
-    two: 2,
-    three: 3,
-    four: 4,
-    five: 5,
-    six: 6,
-    seven: 7,
-    eight: 8,
-    nine: 9
-};
+function collectOperand(button) {
+    let newSpan = document.createElement('span')
 
-let
+    if ( screen.childElementCount == 3 ) evaluate();
 
+    if ( screen.childElementCount == 1 ) screen.appendChild(newSpan);
 
-function collectNumbers (id) {
-    if (operator == undefined) return firstOperand.push(num[id]);
-    if (operator !== undefined) return secondOperand.push(num[id]);
-};
-
-function setOperator(id) {operator = id};
-
-function applyModifier(id) {
-    if (id == 'percent') {
-        console.log('make percentage')
-    };
-
-    if (id == 'plusmn') {
-        if (operator == undefined) return firstOperand.unshift('-');
-        if (operator !== undefined) return secondOperand.unshift('-');
-        return;
-    };
-}
-
-function action(id) {
-    if (id == 'delete') {
-        if (operator == undefined) return firstOperand.pop();
-        if (operator !== undefined) return secondOperand.pop();
-        return;
-    };
-
-    if (id == 'clear') {
-        firstOperand = [];
-        operator = null;
-        secondOperand = [];
-        return;
-    };
-};
-
-const screen = document.querySelector('screen')
-
-function printMath() {
-    const newSpan = document.createElement('span');
-
-    if (screen.lastChild) screen.removeChild(screen.lastChild);
-
-    newSpan.textContent = convertOperandToString(firstOperand)
-}
-
-function convertOperandToString(arr) {
-    return arr.join('').replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    screen.lastChild.textContent = button.textContent;
+    screen.lastChild.classList.add(button.id);
 }
 
 function evaluate() {
-    const a = firstOperand.join('');
+    let a = Number(screen.children[0].textContent);
+    let operand = screen.children[1].classList[0];
+    let b = Number(screen.children[2].textContent);
 
-    const b = secondOperand.join('');
+    let result
 
-    if (operator == 'plus') console.log(a + b);
-    if (operator == 'minus') console.log(a - b);
-    if (operator == 'time') console.log(a * b);
-    if (operator == 'divide') console.log(a / b);
+    switch (operand) {
+        case 'plus':
+            result = a + b;
+            break;
+        case 'minus':
+            result = a - b;
+            break;
+        case 'times':
+            result = a * b;
+            break;
+        case 'divide':
+            result = a / b;
+            break;
+    };
+
+    while (screen.firstChild) screen.removeChild(screen.firstChild);
+    
+    let printResult = document.createElement('span');
+
+    if (isFinite(result)) {
+        screen.id = 'result';
+        printResult.textContent = result;
+    };
+
+    if (result == 'Infinity') {
+        screen.id = 'error';
+        printResult.innerHTML = "F**kin'<br>huge<br>mate!";
+    };
+
+    if (result == 'NaN') {
+        screen.id = 'error';
+        printResult.innerHTML = "Great work!<br>you broke it.";
+    };
+
+    screen.appendChild(printResult);
 };
+
+const evaluateButton = document.querySelector('#equals')
+
+evaluateButton.addEventListener('click', () => {
+    resetScreen();
+    if(screen.childElementCount == 3) evaluate();
+});
+
+const clearButton = document.querySelector('#clear')
+
+clearButton.addEventListener('click', () => {
+    resetScreen();
+    while (screen.firstChild) screen.removeChild(screen.firstChild);
+});
+
+function resetScreen(number) {
+    if (screen.id == 'error') {
+        while (screen.firstChild) screen.removeChild(screen.firstChild);
+    };
+
+    if (screen.id == 'result' && !number) {
+        screen.appendChild(document.createElement('span'))
+    };
+
+    if (screen.id == 'result' && number) {
+        while (screen.firstChild) screen.removeChild(screen.firstChild);
+    };
+    
+    screen.removeAttribute('id');
+}
+
+const negativeButton = document.querySelector('#plusmn')
+
+negativeButton.addEventListener('click', () => {
+        resetScreen();
+        negateNumber();
+    });
+
+function negateNumber() {
+    if (
+        screen.childElementCount == 0 ||
+        screen.childElementCount == 1
+    ) screen.appendChild(document.createElement('span'));
+
+    let currentValue = screen.lastChild.textContent
+
+    if ( currentValue.includes('-') ) return;
+
+    screen.lastChild.textContent = '-' + currentValue;
+}
+
+const percentButton = document.querySelector('#percent')
+
+percentButton.addEventListener('click', () => {
+        resetScreen();
+        percentNumber();
+    });
+
+function percentNumber() {
+    let currentValue = screen.lastChild.textContent;
+
+    screen.lastChild.textContent = currentValue / 100;
+}
